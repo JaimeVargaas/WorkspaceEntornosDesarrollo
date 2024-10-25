7115520ed0f3a441ed70852513804583f1e858c4
JaimeVargaas <jvarbae782@g.educaand.es>
2024-10-25 13:53:35 +0200

    Version 3

---
+++ proyectogit/src/proyectogit/Main3.java
@@ -0,0 +1,13 @@
+package proyectogit;
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola mundo");
+		System.out.println("Version facil");
+		System.out.println("Version 2 facil");
+	}
+
+}
